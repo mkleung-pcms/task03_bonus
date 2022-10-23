eeb33a4dd7bd35ad100bf7ab74f77de58cd95021
mkleung-pcms <116505065+mkleung-pcms@users.noreply.github.com>
2022-10-24 00:10:21 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+input.onButtonPressed(Button.A, function () {
+    cnt += 10
+    basic.showNumber(cnt)
+})
+input.onButtonPressed(Button.AB, function () {
+    while (cnt >= 0) {
+        basic.showNumber(cnt)
+        cnt += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    cnt += 1
+    basic.showNumber(cnt)
+})
+let cnt = 0
+cnt = 0
+basic.showNumber(cnt)
+basic.forever(function () {
+	
+})
